ecf0121a70b341e878ee8640c20d0e0a0014aec9
Ricardo Caldeira <ricardo.caldeira@resultadosdigitais.com.br>
2018-01-04 03:59:38 -0200

    Removes unnecessary constants

--- space-age/space_age.go
+++ space-age/space_age.go
@@ -4,26 +4,19 @@ package space
 type Planet string
 
 const earthYearsInSeconds = 31557600.0
-const mercuryYearsInSeconds = earthYearsInSeconds * 0.2408467
-const venusYearsInSeconds = earthYearsInSeconds * 0.61519726
-const marsYearsInSeconds = earthYearsInSeconds * 1.8808158
-const jupiterYearsInSeconds = earthYearsInSeconds * 11.862615
-const saturnYearsInSeconds = earthYearsInSeconds * 29.447498
-const uranusYearsInSeconds = earthYearsInSeconds * 84.016846
-const neptuneYearsInSeconds = earthYearsInSeconds * 164.79132
 
 // Age takes an age in seconds and a Planet
 // and returns age in years on that planet
 func Age(seconds float64, planet Planet) float64 {
 	planets := map[Planet]float64{
-		"Earth":   earthYearsInSeconds,
-		"Mercury": mercuryYearsInSeconds,
-		"Venus":   venusYearsInSeconds,
-		"Mars":    marsYearsInSeconds,
-		"Jupiter": jupiterYearsInSeconds,
-		"Saturn":  saturnYearsInSeconds,
-		"Uranus":  uranusYearsInSeconds,
-		"Neptune": neptuneYearsInSeconds,
+		"Earth":   1,
+		"Mercury": 0.2408467,
+		"Venus":   0.61519726,
+		"Mars":    1.8808158,
+		"Jupiter": 11.862615,
+		"Saturn":  29.447498,
+		"Uranus":  84.016846,
+		"Neptune": 164.79132,
 	}
-	return seconds / planets[planet]
+	return seconds / (planets[planet] * earthYearsInSeconds)
 }
