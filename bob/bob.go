f9f0751cdf339684cd0ea703ca2b87397fc45c13
Ricardo Caldeira <ricardo.caldeira@resultadosdigitais.com.br>
2018-01-04 00:47:19 -0200

    Fix pending specs

--- bob/bob.go
+++ bob/bob.go
@@ -8,7 +8,7 @@ import (
 
 // Hey takes String questions and returns String responses
 func Hey(remark string) string {
-	reg, _ := regexp.Compile("[^a-zA-Z?]+")
+	reg, _ := regexp.Compile("[^a-zA-Z0-9?]+")
 	remark = reg.ReplaceAllString(remark, "")
 
 	selectedOptions := 0
@@ -19,6 +19,9 @@ func Hey(remark string) string {
 	if isAsking(remark) {
 		selectedOptions += 2
 	}
+	if isSilenced(remark) {
+		selectedOptions += 4
+	}
 
 	switch selectedOptions {
 	case 1:
@@ -27,13 +30,24 @@ func Hey(remark string) string {
 		return "Sure."
 	case 3:
 		return "Calm down, I know what I'm doing!"
+	case 4:
+		return "Fine. Be that way!"
 	}
 
 	return "Whatever."
 }
 
+func isSilenced(remark string) bool {
+	return len(remark) == 0
+}
+
+func isAlpha(remark string) bool {
+	match, _ := regexp.MatchString("[a-zA-Z]+", remark)
+	return match
+}
+
 func isYellin(remark string) bool {
-	return remark == strings.ToUpper(remark) && len(remark) > 1
+	return remark == strings.ToUpper(remark) && len(remark) > 1 && isAlpha(remark)
 }
 
 func isAsking(remark string) bool {
