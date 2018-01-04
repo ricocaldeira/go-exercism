1dac0145aa5e274f8f652dad0cb8b9342fa58ac2
Ricardo Caldeira <ricardo.caldeira@resultadosdigitais.com.br>
2018-01-04 18:12:06 -0200

    Refactor to loop once

--- scrabble-score/scrabble.go
+++ scrabble-score/scrabble.go
@@ -5,14 +5,14 @@ import (
 	"strings"
 )
 
-var letterScorings = map[string]int{
-	"AEIOULNRST": 1,
-	"DG":         2,
-	"BCMP":       3,
-	"FHVWY":      4,
-	"K":          5,
-	"JX":         8,
-	"QZ":         10,
+var letterScorings = map[rune]int{
+	'A': 1, 'E': 1, 'I': 1, 'O': 1,
+	'U': 1, 'L': 1, 'N': 1, 'R': 1,
+	'S': 1, 'T': 1, 'D': 2, 'G': 2,
+	'B': 3, 'C': 3, 'M': 3, 'P': 3,
+	'F': 4, 'H': 4, 'V': 4, 'W': 4,
+	'Y': 4, 'K': 5, 'J': 8, 'X': 8,
+	'Q': 10, 'Z': 10,
 }
 
 // Score takes a word String and returns its score int
@@ -20,11 +20,7 @@ func Score(word string) int {
 	word = strings.ToUpper(word)
 	scrabbleScore := 0
 	for _, char := range word {
-		for letter, value := range letterScorings {
-			if strings.ContainsRune(letter, char) {
-				scrabbleScore += value
-			}
-		}
+		scrabbleScore += letterScorings[char]
 	}
 	return scrabbleScore
 }
