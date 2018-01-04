adb22cba0b6436f2d96d360edb7228626b9214c2
Ricardo Caldeira <ricardo.caldeira@resultadosdigitais.com.br>
2018-01-04 19:10:58 -0200

    Simplifies algorithm

--- reverse-string/reverse.go
+++ reverse-string/reverse.go
@@ -3,9 +3,9 @@ package reverse
 
 // String takes a string and returns its reverse
 func String(sentence string) string {
-	runes := []rune(sentence)
-	for i, j := 0, len(runes)-1; i < j; i, j = i+1, j-1 {
-		runes[i], runes[j] = runes[j], runes[i]
+	reverseSentence := ""
+	for _, char := range sentence {
+		reverseSentence = string(char) + reverseSentence
 	}
-	return string(runes)
+	return reverseSentence
 }
