1d9662e9bd9730d338cd985613073e781dce96db
Ricardo Caldeira <ricardo.caldeira@resultadosdigitais.com.br>
2018-01-06 05:03:52 -0200

    Finish clock

---
+++ clock/clock.go
@@ -0,0 +1,33 @@
+// Package clock implements a clock without dates
+package clock
+
+import (
+	"fmt"
+	"time"
+)
+
+// Clock has hour int and minute int
+type Clock struct {
+	hour   int
+	minute int
+}
+
+func (clock Clock) String() string {
+	return fmt.Sprintf("%02d:%02d", clock.hour, clock.minute)
+}
+
+// Add takes an integer interval, add it to clock and return new clock time
+func (clock Clock) Add(interval int) Clock {
+	date := time.Date(2000, time.January, 1, clock.hour, clock.minute, 0, 0, time.UTC)
+	date = date.Add(time.Minute * time.Duration(interval))
+	clock.hour, clock.minute = date.Hour(), date.Minute()
+	return clock
+}
+
+// New takes integers hours and minutes and returns a clock time
+func New(hour int, minute int) Clock {
+	date := time.Date(2000, time.January, 1, hour, minute, 0, 0, time.UTC)
+	timeHour := Clock{date.Hour(), date.Minute()}
+
+	return timeHour
+}
