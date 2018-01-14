8b56a92c4cb2dd0a288f8ebb734c71c0093d056c
Ricardo Caldeira <ricardo.caldeira@resultadosdigitais.com.br>
2018-01-14 02:46:00 -0200

    Refactor on Flatten()

--- flatten-array/flatten.go
+++ flatten-array/flatten.go
@@ -1,30 +1,17 @@
 // Package flatten implements functions to flatten arrays
 package flatten
 
-import (
-	"reflect"
-)
-
 // Flatten takes an interface and returns a flatten array
-func Flatten(nested interface{}) []interface{} {
-	flattened := []interface{}{}
-	nestedSlice, ok := nested.([]interface{})
-
-	if !ok {
-		return flattened
-	}
-
-	for _, e := range nestedSlice {
-		t := reflect.TypeOf(e)
-		if t == nil {
-			continue
-		}
-		switch kind := t.Kind(); kind {
-		case reflect.Slice:
-			flattened = append(flattened, Flatten(e)...)
-		default:
-			flattened = append(flattened, e)
+func Flatten(input interface{}) []interface{} {
+	output := []interface{}{}
+	for _, val := range input.([]interface{}) {
+		switch val.(type) {
+		case []interface{}:
+			tmp := Flatten(val)
+			output = append(output, tmp...)
+		case int:
+			output = append(output, val)
 		}
 	}
-	return flattened
+	return output
 }
