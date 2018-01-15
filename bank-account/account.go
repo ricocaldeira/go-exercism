398077b3fa5eddf7cfb2b460332c07c08b09b1c0
Ricardo Caldeira <ricardo.caldeira@resultadosdigitais.com.br>
2018-01-15 19:57:50 -0200

    Finish bank-account mutex

--- bank-account/account.go
+++ bank-account/account.go
@@ -1,37 +1,56 @@
 // Package account implements functions to deal with bank accounts
 package account
 
+import (
+	"sync"
+)
+
 type Account struct {
-	balance int64
-	status  string
-}
-
-func (acc *Account) Close() (payout int64, ok bool) {
-	acc.status = "closed"
-	return acc.balance, !ok
-}
-
-func (acc *Account) Balance() (balance int64, ok bool) {
-	ok = acc.status != "closed"
-	if acc.balance < 0 {
-		acc.balance = 0
-	}
-
-	return acc.balance, ok
-}
-
-func (acc *Account) Deposit(amount int64) (newBalance int64, ok bool) {
-	acc.balance += amount
-	ok = acc.balance >= 0 && acc.status != "closed"
-	return acc.balance, ok
+	balance  int64
+	isClosed bool
+	mutex    sync.Mutex
 }
 
 func Open(initialDeposit int64) *Account {
 	if initialDeposit < 0 {
 		return nil
 	}
-	acc := new(Account)
-	acc.balance = initialDeposit
-	acc.status = "opened"
-	return acc
+	return &Account{balance: initialDeposit, isClosed: false}
+}
+func (a *Account) Close() (payout int64, ok bool) {
+	a.mutex.Lock()
+	if a.isClosed {
+		a.mutex.Unlock()
+		return 0, false
+	}
+	a.isClosed = true
+	payout, a.balance = a.balance, 0
+	a.mutex.Unlock()
+	return payout, true
+
+}
+func (a *Account) Balance() (balance int64, ok bool) {
+	a.mutex.Lock()
+	if a.isClosed {
+		a.mutex.Unlock()
+		return 0, false
+	}
+	balance = a.balance
+	a.mutex.Unlock()
+	return balance, true
+}
+func (a *Account) Deposit(amount int64) (newBalance int64, ok bool) {
+	a.mutex.Lock()
+	if a.isClosed {
+		a.mutex.Unlock()
+		return 0, false
+	}
+	newBalance = a.balance + amount
+	if newBalance < 0 {
+		a.mutex.Unlock()
+		return 0, false
+	}
+	a.balance += amount
+	a.mutex.Unlock()
+	return newBalance, true
 }
