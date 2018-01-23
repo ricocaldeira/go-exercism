9f592311ebddd686dfe5e5e5830c9dc2622d8a18
Ricardo Caldeira <ricardo.caldeira@resultadosdigitais.com.br>
2018-01-23 01:37:53 -0200

    Finish etl

---
+++ etl/etl.go
@@ -0,0 +1,32 @@
+package etl
+
+import (
+	"strings"
+)
+
+// Transform formats oldMap data into newMap format like
+//
+// given{
+// 	1: {"A", "E"},
+// 	2: {"D", "G"},
+// },
+//
+// expectation{
+// 	"a": 1,
+// 	"e": 1,
+// 	"d": 2,
+// 	"g": 2,
+// },
+//
+func Transform(oldMap map[int][]string) map[string]int {
+	var newMap = map[string]int{}
+	for key, strCollection := range oldMap {
+		for _, str := range strCollection {
+			lowStr := strings.ToLower(str)
+			if _, ok := newMap[lowStr]; !ok {
+				newMap[lowStr] = key
+			}
+		}
+	}
+	return newMap
+}
