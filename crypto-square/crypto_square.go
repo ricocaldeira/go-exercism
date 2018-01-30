22d4c84626eb2d179ca5487b26b4c869a6d6d2a1
Ricardo Caldeira <ricardo.caldeira@resultadosdigitais.com.br>
2018-01-29 22:37:51 -0200

    Finish crypto-square

--- crypto-square/crypto_square.go
+++ crypto-square/crypto_square.go
@@ -2,19 +2,25 @@
 package cryptosquare
 
 import (
+	"strings"
 	"unicode"
 )
 
+// Encode Implement the classic method for composing secret messages called a square code.
 func Encode(message string) string {
 	encodedMessage := ""
-	for _, r := range message {
-		if unicode.IsLetter(r) {
-			encodedMessage = encodedMessage + string(r)
+	for _, l := range strings.ToLower(message) {
+		if unicode.IsDigit(l) || unicode.IsLetter(l) {
+			encodedMessage = encodedMessage + string(l)
 		}
 	}
-
-	// Calculate number of columns
-	// c: columns r: rows
-	// such that `c >= r` and `c - r <= 1`
-	return encodedMessage
+	edge := 1
+	for edge*edge < len(encodedMessage) {
+		edge++
+	}
+	square := make([]string, edge)
+	for i, c := range encodedMessage {
+		square[i%edge] += string(c)
+	}
+	return strings.Join(square, " ")
 }
