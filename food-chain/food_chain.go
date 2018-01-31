17560f351a49214945b0b3cd894efa5c0957c2e9
Ricardo Caldeira <ricardo.caldeira@resultadosdigitais.com.br>
2018-01-30 23:07:43 -0200

    Started food-chain

---
+++ food-chain/food_chain.go
@@ -0,0 +1,19 @@
+// Package foodchain implements functions to output lyrics
+// of the song 'I Know an Old Lady Who Swallowed a Fly'.
+package foodchain
+
+// Verse returns a verse of song
+func Verse(n int) string {
+	return ""
+}
+
+// Verses returns verses of song
+func Verses(start int, end int) string {
+	return ""
+}
+
+// Song returns lyrics of
+// 'I Know an Old Lady Who Swallowed a Fly' song
+func Song() string {
+	return ""
+}
