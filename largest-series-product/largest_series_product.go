5d78c5a15a0f96853b76885216b327645f9b5b19
Ricardo Caldeira <ricardo.caldeira@resultadosdigitais.com.br>
2018-02-12 00:26:28 -0200

    Add documentation

--- largest-series-product/largest_series_product.go
+++ largest-series-product/largest_series_product.go
@@ -1,5 +1,9 @@
+// Package lsproduct implements fuctions to to calculate largest series products
 package lsproduct
 
+// LargestSeriesProduct Given a string of digits, 
+// calculate the largest product for a contiguous
+// substring of digits of length n.
 func LargestSeriesProduct(series string, n int) (int, error) {
 	return 0, nil
 }
