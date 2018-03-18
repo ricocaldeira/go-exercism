214caa0781e5334311c5a5438b6516472ecc7cf7
Ricardo Caldeira <ricardo.caldeira@resultadosdigitais.com.br>
2018-03-18 13:56:38 -0300

    Pass case 0123456789

--- largest-series-product/largest_series_product.go
+++ largest-series-product/largest_series_product.go
@@ -1,12 +1,23 @@
 // Package lsproduct implements fuctions to to calculate largest series products
 package lsproduct
 
+import "strconv"
+
 // LargestSeriesProduct Given a string of digits,
 // calculate the largest product for a contiguous
 // substring of digits of length n.
 func LargestSeriesProduct(series string, n int) (int, error) {
-	if series == "0123456789" {
-		return 72, nil
+	largestProduct := 0
+	for i := 0; i < len(series); i = i + n {
+		numbers := series[i : i+n]
+		mul := 1
+		for j := 0; j < len(numbers); j++ {
+			intNum, _ := strconv.Atoi(numbers[j : j+1])
+			mul = mul * intNum
+		}
+		if mul > largestProduct {
+			largestProduct = mul
+		}
 	}
-	return 18, nil
+	return largestProduct, nil
 }
