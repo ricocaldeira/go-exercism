c58a323300a9e9759f5322087304e63e2871dcde
Ricardo Caldeira <ricardo.caldeira@resultadosdigitais.com.br>
2018-04-01 20:49:09 -0300

    Starts sieve.go

---
+++ sieve/sieve.go
@@ -0,0 +1,7 @@
+// Package sieve implements functions to deal with prime numbers
+package sieve
+
+// Sieve returns all the primes from 2 up to a given number
+func Sieve(n int) []int {
+	return []int{0}
+}
