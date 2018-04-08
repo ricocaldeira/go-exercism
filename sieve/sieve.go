686d1b0e6741953ed0786d3d9c68c64c1589fbc3
Ricardo Caldeira <ricardo.caldeira@resultadosdigitais.com.br>
2018-04-08 12:59:21 -0300

    Finish sieve.go

--- sieve/sieve.go
+++ sieve/sieve.go
@@ -1,10 +1,25 @@
 // Package sieve implements functions to deal with prime numbers
 package sieve
 
+import (
+	"math"
+)
+
 // Sieve returns all the primes from 2 up to a given number
 func Sieve(n int) []int {
 	if n < 2 {
 		return []int{}
 	}
-	return []int{2}
+	primeNumbers := []int{2}
+	for i := 3; i <= n; i++ {
+		for k := 2; k <= i; k++ {
+			if k == i {
+				primeNumbers = append(primeNumbers, i)
+			}
+			if math.Mod(float64(i), float64(k)) == 0 {
+				break
+			}
+		}
+	}
+	return primeNumbers
 }
