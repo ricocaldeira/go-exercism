810143c468d5e53fda9f8031a61d11014796946b
Ricardo Caldeira <ricardo.caldeira@resultadosdigitais.com.br>
2019-10-25 00:25:17 -0300

    Passing more test cases

--- word-count/word_count.go
+++ word-count/word_count.go
@@ -1,9 +1,17 @@
 package wordcount
 
+import "strings"
+
 // Frequency maps words and ocurrencies count
 type Frequency map[string]int
 
 // WordCount counts words frequency and returns Frequency type
 func WordCount(phrase string) Frequency {
-	return Frequency{"word": 1}
+	words := strings.Split(phrase, " ")
+	frequency := Frequency{}
+
+	for _, word := range words {
+		frequency[word]++
+	}
+	return frequency
 }
